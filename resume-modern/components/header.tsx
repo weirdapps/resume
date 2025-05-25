3448ef28b210dc7baeeda0b83b92fc9dc7f26bb8
Dimitris <plessasdimitrios@yahoo.com>
2025-05-25 20:32:42 +0300

    OCD push

--- resume-modern/components/header.tsx
+++ resume-modern/components/header.tsx
@@ -29,7 +29,7 @@ export default function Header() {
             transition={{ delay: 0.3, duration: 0.5 }}
             className="text-xl text-gray-600 dark:text-gray-300 mt-2 text-center md:text-left"
           >
-            engineer by training, banker by trade, ex McKinsey consultant
+            Engineer by Training, Banker by Trade, ex McKinsey
           </motion.p>
           
           <motion.div
